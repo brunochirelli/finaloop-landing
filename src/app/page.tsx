3102a3d3d4aa6931dc0299eb281b6fa7b1c12936
Bruno Chirelli <brunochirelli@gmail.com>
2024-09-02 18:05:42 -0300

    add shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/Hero/Hero";
+import LogosCarousel from "@/components/LogosCarousel/LogosCarousel";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <LogosCarousel />
     </>
   );
 }
